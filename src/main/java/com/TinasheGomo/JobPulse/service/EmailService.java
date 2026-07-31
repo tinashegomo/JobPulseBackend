d946920fbf6a27b25ac112a9de57d69695a57b41
Tinashe Gomo <tinashegomo96@gmail.com>
2026-07-31 19:19:21 +0200

    deleted some files

--- src/main/java/com/TinasheGomo/JobPulse/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.TinasheGomo.JobPulse.service;
-
-public interface EmailService {
-    void sendVerificationEmail(String to, String token);
-    void sendPasswordResetEmail(String to, String token);
-}
